a9164f1c2b8883b01880d74653d3e0b2374e10b4
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-22 10:00:20 +0800

    mp_orders插件移动到根站点插件下

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+defined('IN_ECJIA') or exit('No permission resources.');
+return array(
+    'ext_code'      => 'mp_orders',
+			
+	'forms' => array(
+	),
+);
+
+// end
---
+++ languages/zh-cn/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * 订单查询语言文件
+ */
+defined('IN_ECJIA') or exit('No permission resources.');
+
+
+// end
