86f13dabf65208ccaaf80f5273107ba04949b387
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-06-15 14:43:04 +0800

    统一升级新的公众平台插件

--- mp_orders.php
+++ mp_orders.php
@@ -77,7 +77,12 @@ class plugin_mp_orders {
     }
 }
 
+Ecjia_PluginManager::extend('mp_orders', function() {
+    require_once RC_Plugin::plugin_dir_path(__FILE__) . 'mp_orders.class.php';
+    return new mp_orders();
+});
+
 RC_Plugin::register_activation_hook(__FILE__, array('plugin_mp_orders', 'install'));
 RC_Plugin::register_deactivation_hook(__FILE__, array('plugin_mp_orders', 'uninstall'));
-RC_Hook::add_filter('platform_factory_adapter_instance', array( 'plugin_mp_orders', 'adapter_instance' ), 10, 2);
+// RC_Hook::add_filter('platform_factory_adapter_instance', array( 'plugin_mp_orders', 'adapter_instance' ), 10, 2);
 // end
