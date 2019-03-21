d43b4d26ff7630d0dad1a2c4798a07fee5a68e4c
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-21 18:35:24 +0800

    更新mp_orders插件的语言包模板提取

--- mp_orders.php
+++ mp_orders.php
@@ -73,6 +73,7 @@ class plugin_mp_orders {
 }
 
 Ecjia_PluginManager::extend('mp_orders', function() {
+    RC_Locale::loadPluginTextdomain('mp_orders');
     require_once RC_Plugin::plugin_dir_path(__FILE__) . 'mp_orders.class.php';
     return new mp_orders();
 });
