2fa0f8e18a403cd0a7aa8623c77252bf0952520f
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-28 15:09:26 +0800

    补充插件中遗漏的语言包

--- mp_orders.class.php
+++ mp_orders.class.php
@@ -111,10 +111,10 @@ class mp_orders extends PlatformAbstract
 
             if (empty($orders)){
                 $articles = [
-                    'Title' => __('订单查询', 'mp_orders'),
-                    'Description' => __('您目前还没有消费过哦', 'mp_orders'),
+                    'Title' 		=> __('订单查询', 'mp_orders'),
+                    'Description' 	=> __('您目前还没有消费过哦', 'mp_orders'),
                     'Url'           => '',
-                    'PicUrl' => '',
+                    'PicUrl' 		=> '',
                 ];
                 return WechatRecord::News_reply($this->getMessage(), $articles['Title'], $articles['Description'], $articles['Url'], $articles['PicUrl']);
             }
@@ -124,9 +124,9 @@ class mp_orders extends PlatformAbstract
                 //https://cityo2o.ecjia.com/sites/m/index.php?m=user&c=order&a=order_detail&order_id=55347&type=detail
 
                 $article = [
-                    'Title' => sprintf(__("【%s】订单号：%s", 'mp_orders'), $orderinfo['label_order_status'], $orderinfo['order_sn']),
-                    'Url' => str_replace('sites/platform/', 'sites/m/', RC_Uri::url('user/order/order_detail', array('order_id' => $orderinfo['order_id'], 'type' => detail))),
-                    'PicUrl' => $orderinfo['goods_list'][0]['img']['small'],
+                    'Title' 	=> sprintf(__("【%s】订单号：%s", 'mp_orders'), $orderinfo['label_order_status'], $orderinfo['order_sn']),
+                    'Url' 		=> str_replace('sites/platform/', 'sites/m/', RC_Uri::url('user/order/order_detail', array('order_id' => $orderinfo['order_id'], 'type' => detail))),
+                    'PicUrl' 	=> $orderinfo['goods_list'][0]['img']['small'],
                 ];
 
                 $articles[$key] = WechatRecord::News_reply($this->getMessage(), $article['Title'],'', $article['Url'], $article['PicUrl']);
